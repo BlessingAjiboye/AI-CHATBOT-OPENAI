671831daf06b6acfffbd47efeafcdd05cc9c60da
Blessing Ajiboye <blessjiboye@yahoo.com>
2024-12-16 23:12:09 -0500

    first commit

---
+++ .history/backend/index_20241216230034.js
@@ -0,0 +1,55 @@
+/*import { Configuration, OpenAIApi } from "openai";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+const port = 8000;
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+  });
+*/
+
+//import { Configuration, OpenAIApi } from "openai";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+const port = 8000;
+app.use(bodyParser.json());
+app.use(cors());
+
+
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  //API key Here
+});
+
+
+app.post("/", async (request, response) => {
+  const { chats } = request.body;
+
+  const result = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are a GPT. You can help with graphic design tasks",
+      },
+      ...chats,
+    ],
+  });
+
+  response.json({
+    output: result.data.choices[0].message,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
